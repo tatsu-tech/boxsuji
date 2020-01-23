649b00e58ee9948bb0c50749a58d55e2e73dd496
tatsu-tech <55968601+tatsu-tech@users.noreply.github.com>
2020-01-23 23:13:52 +0900

    cssの細かな修正

--- app/assets/javascripts/fade.js
+++ app/assets/javascripts/fade.js
@@ -2,13 +2,13 @@ $(document).on('turbolinks:load', function() {
   $(".game-screen__btn-lists--description").on("click", "#description", function() {
     $(".description-screen").fadeToggle();
   })
-  $(".topbackground").on("click", ".description-screen", function() {
+  $(".description-screen").on("click", ".description-screen__backbtn", function() {
     $(".description-screen").fadeToggle();
   })
   $(".game-screen__quiz-lists").on("click", ".game-screen__quiz-lists--created-game", function() {
     $(".created-screen").fadeToggle();
   })
-  $(".topbackground").on("click", ".created-screen__backbtn", function() {
+  $(".created-screen").on("click", ".created-screen__backbtn", function() {
     $(".created-screen").fadeToggle();
   })
   setTimeout("$('.game-screen__notice-message').fadeOut('slow')", 1500)
